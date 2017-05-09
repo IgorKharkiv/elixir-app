9e2b81cf038b95442839626a9b25743c57df7000
igor.syrov <syrov.igor1988@gmail.com>
2017-05-09 23:12:42 +0300

    add components, assets and templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,45 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// import route modules
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+
+//import page components
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+// array of routes
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'home', component: CartComponent },
+  { path: 'home', component: ContactComponent },
+  { path: 'home', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    HomeComponent,
+    CartComponent,
+    CheckoutComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
